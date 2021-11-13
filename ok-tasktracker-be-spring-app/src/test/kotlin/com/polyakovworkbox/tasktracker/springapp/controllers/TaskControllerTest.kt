e566fa345fdf38a0833d0c64f22ca5477963840a
sinister <polyakovworkbox@gmail.com>
2021-11-13 14:27:21 +0300

    m8-security: auth failure test added

--- ok-tasktracker-be-spring-app/src/test/kotlin/com/polyakovworkbox/tasktracker/springapp/controllers/TaskControllerTest.kt
+++ ok-tasktracker-be-spring-app/src/test/kotlin/com/polyakovworkbox/tasktracker/springapp/controllers/TaskControllerTest.kt
@@ -29,6 +29,7 @@ import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 
 
@@ -45,6 +46,40 @@ class TaskControllerTest {
     private lateinit var tokenRetriever: TokenRetriever
 
     private val om = ObjectMapper().registerKotlinModule()
+
+    @Test
+    fun `wrong token failure`() {
+        val request = CreateTaskRequest(
+            requestId = "1234567890",
+            debug = Debug(Debug.Mode.STUB, Debug.Stub.SUCCESS),
+            task = CreatableTask(
+                name = "some name",
+                ownerId = "00560000-0000-0000-0000-000000000001",
+                description = "some description"
+            )
+        )
+
+        val headers = HttpHeaders()
+        headers.setBearerAuth(getFailureToken())
+        headers.accept = listOf(MediaType.APPLICATION_JSON)
+        headers.contentType = MediaType.APPLICATION_JSON
+
+        val entity: HttpEntity<String> = HttpEntity<String>(om.writeValueAsString(request), headers)
+
+        val response = restTemplate
+            .exchange(
+                "http://localhost:${port}/task/create",
+                HttpMethod.POST,
+                entity,
+                CreateTaskResponse::class.java
+            )
+
+        assertEquals(HttpStatus.UNAUTHORIZED, response.statusCode)
+        assertEquals(null, response.body)
+    }
+
+
+
 
     @Test
     fun `create stub success`() {
@@ -70,14 +105,6 @@ class TaskControllerTest {
 
         assertEquals(ResponseResult.SUCCESS, response?.result)
         assertEquals("""read "The Lord of the Rings"""", response?.task?.name)
-    }
-
-    private fun httpHeaders(): HttpHeaders {
-        val headers = HttpHeaders()
-        headers.setBearerAuth(tokenRetriever.getToken())
-        headers.accept = listOf(MediaType.APPLICATION_JSON)
-        headers.contentType = MediaType.APPLICATION_JSON
-        return headers
     }
 
     @Test
@@ -304,5 +331,28 @@ class TaskControllerTest {
         assertEquals(ResponseResult.ERROR, response?.result)
         assertThat(response?.availableTasks.isNullOrEmpty())
         assertTrue(response?.errors?.all { it.message == "DB error occurred" } ?: false)
+    }
+
+    private fun httpHeaders(): HttpHeaders {
+        val headers = HttpHeaders()
+        headers.setBearerAuth(tokenRetriever.getToken())
+        headers.accept = listOf(MediaType.APPLICATION_JSON)
+        headers.contentType = MediaType.APPLICATION_JSON
+        return headers
+    }
+
+    private fun getFailureToken(): String {
+        return "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJfMEdTWGpHaTNybEdlcWhYTzU4djlTTTlnNkpzY0hTX2JpSm5mdmJjam00In0." +
+                "eyJleHAiOjE2MzY1ODA3NjksImlhdCI6MTYzNjU4MDQ2OSwianRpIjoiYmFlM2U5NTEtMWI5ZC00MjFhLWJkZTUtOGQ5MmU2YjcwNTE4IiwiaX" +
+                "NzIjoiaHR0cDovL2xvY2FsaG9zdDo4NDg0L2F1dGgvcmVhbG1zL215X3JlYWxtIiwiYXVkIjoicmVhbG0tbWFuYWdlbWVudCIsInN1YiI6IjM2" +
+                "YzhiZGE0LTAwM2UtNDVkOC04N2MzLTRlOTcwZmE2ODYyYyIsInR5cCI6IkJlYXJlciIsImF6cCI6Im15X2NsaWVudCIsInNlc3Npb25fc3RhdG" +
+                "UiOiI3MWVjMTZjYi1jNmYxLTQ1YWUtODk5Ni1jNzI3ZTk1OWQwYTYiLCJhY3IiOiIxIiwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxp" +
+                "bmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iLCJVU0VSIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsicmVhbG0tbWFuYWdlbWVudCI6eyJyb2" +
+                "xlcyI6WyJ2aWV3LXVzZXJzIiwicXVlcnktZ3JvdXBzIiwicXVlcnktdXNlcnMiXX19LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwi" +
+                "c2lkIjoiNzFlYzE2Y2ItYzZmMS00NWFlLTg5OTYtYzcyN2U5NTlkMGE2IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJwcmVmZXJyZWRfdXNlcm" +
+                "5hbWUiOiJ1c2VyIn0.qDYu0fwmQfVkoJrHQCXLbzDOkyaikRmDqF1ALKcEIvEV-ux3TRFm9-opnrMmrZRJCH3vhrU2eXF-VgzOwvRKWBqR1as1" +
+                "wNBYB_nxyIPNoo1Qh8QtAhzh36CYvb-7-E6yrFDTdJydYdYzYIYFGlXpW2cCFfKuNz5AFt88qfyUjhNTFfynZ79YAxVypA5nqHrfzxD4VnM21T" +
+                "pwlo1Uc1OcSuKErgq8gKjRUaauf2tM4qQC8XEEudmdebrIEU1j3itRzbJfwpwIagPgJVPYBWfQxTpwqrF_SIkHoY4xl7GkeE1CGH-fpwUeYBPA" +
+                "QoO-pvD_W3IboGdyXS4dFNEEh3vwyA"
     }
 }
